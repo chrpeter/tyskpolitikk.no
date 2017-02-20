e8c842adbef46608bd20a16cefef1f02df642164
Christian Peter <christian.peter@knowit.no>
2017-02-20 20:41:05 +0100

    updates env

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
-    aws_secret_access_key: ENV['AWS_SECRET_KEY'],                        # required
+    aws_access_key_id:     ENV['AWS_TYSK_ACCESS_KEY_ID'],                        # required
+    aws_secret_access_key: ENV['AWS_TYSK_SECRET_KEY'],                        # required
     region:                'eu-central-1',                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'tyskpolitikk'                          # required
