2e7fc642fb70412ff41a4591b60306a6806f3c69
Christian Peter <christian.peter@knowit.no>
2017-05-01 17:22:43 +0200

    Adds tinymce toolbar

--- app/controllers/comment_articles_controller.rb
+++ app/controllers/comment_articles_controller.rb
@@ -1,7 +1,7 @@
 class CommentArticlesController < ApplicationController
   load_and_authorize_resource :comment_article
   def index
-    @comment_articles = CommentArticle.public_article.paginate(:page => params[:page])
+    @comment_articles = CommentArticle.public_article.order('created_at DESC').paginate(:page => params[:page])
   end
   def show
     @comment_articles = BaseArticle.public_article.where.not(id: @comment_article.id).order('created_at DESC').limit(5)
