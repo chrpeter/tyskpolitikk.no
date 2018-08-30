09de001a3dc5815a726277dcd8401fef48852083
Christian Peter <christian150790@gmail.com>
2018-08-30 23:17:11 +0200

    Tries to fix url problem

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,17 @@ class ImagesController < ApplicationController
   def show
     path = params[:path]
     format = params[:format]
-    file = open(URI.parse("#{ENV['AWS_S3_URL']}#{path}.#{format}"))
+    file = open(normalize_uri("#{ENV['AWS_S3_URL']}#{path}.#{format}"))
     send_file file , type: "image/#{format}", disposition: "inline"
+  end
+
+  private
+  def normalize_uri(uri)
+    return uri if uri.is_a? URI
+
+    uri = uri.to_s
+    uri, *tail = uri.rpartition "#" if uri["#"]
+
+    URI(URI.encode(uri) << Array(tail).join)
   end
 end
